0cbc6cd83b31d48a41ddbaa01e3b89ded6f178e9
adimehta9 <52846329+adimehta9@users.noreply.github.com>
2022-11-27 23:20:02 -0500

    Create board.h

---
+++ board.h
@@ -0,0 +1,33 @@
+#include "player.h"
+#include "display.h"
+#include "graphics.h"
+#include "boardObjects.h"
+#include <memory>
+
+
+#ifndef BOARD__H
+#define BOARD__H
+
+
+class Board{
+    shared_ptr<Player> one;
+    shared_ptr<Player> two;
+    shared_ptr<Display> dis;
+    Graphics graph;
+    bool graphics;
+    
+    public:
+    
+    Board(shared_ptr<Player> one, shared_ptr<Player> two, bool graphics);
+    void showBoard(bool one_turn);
+
+
+
+
+};
+
+
+
+
+
+#endif
