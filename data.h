e3a5eac4d4945911730d81cc8a111dd714bbdf1d
adimehta9 <52846329+adimehta9@users.noreply.github.com>
2022-11-27 23:21:32 -0500

    Create data.h

---
+++ data.h
@@ -0,0 +1,18 @@
+#ifndef DATA__H
+#define DATA__H
+
+#include "link.h"
+using namespace std;
+
+class Data: public Link{
+
+    public:
+    Data(char c, int x, int y, int owner, int strength);
+    char getType() override;
+    int getStrength();
+
+};
+
+
+
+#endif
